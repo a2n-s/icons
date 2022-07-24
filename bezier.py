39c6a047f25098cc04bca39f29b001d8da8f3ffd
a2n-s <44101798+AntoineStevan@users.noreply.github.com>
2022-07-24 18:34:04 +0200

    make output types better and clearer

--- bezier.py
+++ bezier.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 Point = np.ndarray[float, float]
 
 
-def bezier_1(point_0: Point, point_1: Point, *, nb_points: int = 1000):
+def bezier_1(point_0: Point, point_1: Point, *, nb_points: int = 1000) -> np.ndarray:
     curve = []
     x_0, y_0 = point_0
     x_1, y_1 = point_1
@@ -14,10 +14,10 @@ def bezier_1(point_0: Point, point_1: Point, *, nb_points: int = 1000):
         x = x_0 * t + x_1 * (1 - t)
         y = y_0 * t + y_1 * (1 - t)
         curve.append((x, y))
-    return curve
+    return np.array(curve)
 
 
-def bezier_2(point_0: Point, point_1: Point, point_2: Point, *, nb_points: int = 1000):
+def bezier_2(point_0: Point, point_1: Point, point_2: Point, *, nb_points: int = 1000) -> np.ndarray:
     curve = []
     steps = np.linspace(0, 1, nb_points)
     first = bezier_1(point_0, point_1, nb_points=1000)
@@ -26,7 +26,7 @@ def bezier_2(point_0: Point, point_1: Point, point_2: Point, *, nb_points: int =
         x = x_01 * t + x_12 * (1 - t)
         y = y_01 * t + y_12 * (1 - t)
         curve.append((x, y))
-    return curve
+    return np.array(curve)
 
 
 A = np.array([0, 0])
