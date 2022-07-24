73e916a826c149c90cf2db2c5d5781abff129e02
a2n-s <44101798+AntoineStevan@users.noreply.github.com>
2022-07-24 18:21:20 +0200

    add second order bezier

--- bezier.py
+++ bezier.py
@@ -16,11 +16,36 @@ def bezier_1(point_0: Point, point_1: Point, *, nb_points: int = 1000):
     return xs, ys
 
 
+def bezier_2(point_0: Point, point_1: Point, point_2: Point, *, nb_points: int = 1000):
+    xs, ys = [], []
+    x_0, y_0 = point_0
+    x_1, y_1 = point_1
+    x_2, y_2 = point_2
+    for t in np.linspace(0, 1, nb_points):
+        x_01 = x_0 * t + x_1 * (1 - t)
+        y_01 = y_0 * t + y_1 * (1 - t)
+        x_12 = x_1 * t + x_2 * (1 - t)
+        y_12 = y_1 * t + y_2 * (1 - t)
+        xs.append(x_01 * t + x_12 * (1 - t))
+        ys.append(y_01 * t + y_12 * (1 - t))
+    return xs, ys
+
+
 A = np.array([0, 0])
 B = np.array([1, .5])
-
-x, y = bezier_1(A, B, nb_points=1000)
+C = np.array([.5, .5])
 
 plt.style.use("dark_background")
+
+x, y = bezier_1(A, B, nb_points=1000)
 plt.plot(x, y)
+plt.scatter(*A)
+plt.scatter(*B)
+
+x, y = bezier_2(A, B, C, nb_points=1000)
+plt.plot(x, y)
+plt.scatter(*A)
+plt.scatter(*B)
+plt.scatter(*C)
+
 plt.show()
